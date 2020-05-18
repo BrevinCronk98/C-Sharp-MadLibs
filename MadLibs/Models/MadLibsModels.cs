0262b85f2f399f1cb9790c9ddfb6fe20d981999d
brevin <cronkbrevin98@gmail.com>
2020-05-18 12:03:15 -0700

    Lunch Commit

---
+++ MadLibs/Models/MadLibsModels.cs
@@ -0,0 +1,20 @@
+
+namespace MadLibs.Models
+{
+    public class MadLibs
+    {
+        public string NounOne {get; set; }
+        public string NounTwo {get; set; }
+        public string NounThree {get; set; }
+        public string VerbOne { get; set; }
+        public string VerbTwo {get; set; }
+        public string VerbThree { get; set;}
+        public string AdjOne { get; set; }
+        public string AdjTwo { get; set; }
+        public string AdjThree { get; set;}
+        public string AdverbOne { get; set;}
+        public string AdverbTwo { get; set;}
+        public string AdverbThree { get; set;}
+    }
+    
+}
